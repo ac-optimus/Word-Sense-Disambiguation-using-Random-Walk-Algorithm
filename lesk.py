d86072972b0315b9536f56cd81a8e10e4bbc71d5
ac-optimus <abhavya.chandra@iitgn.ac.in>
2018-12-05 13:27:30 +0530

    adding lesk score code

---
+++ lesk.py
@@ -0,0 +1,43 @@
+#lesk without applying stemming or lemmentisation
+
+def lesk(sense1,sense2):#takes two glosses as input
+    sent1 = sense1.split()
+    sent2 = sense2.split()
+    incmt1=0
+    incmt2=0
+    leskScr = 0
+    count = 0
+   # flag = 0 #this is see if last word was same or not
+    while (incmt1<len(sent1)-1) :
+        if incmt2 == len(sent2):#can shift this in the else
+            #set it to zero
+            incmt2 = 0
+            incmt1 = incmt1 + 1
+        
+        if  sent1[incmt1] == sent2[incmt2] :
+            #then start the itteration, dude
+            
+            count = count + 1
+    #        flag = 1  #use count codition instead.
+            
+            incmt1 = incmt1 + 1
+            incmt2 = incmt2 + 1
+        
+        else:
+            
+            if count>0:#the termination of last ngram found
+                leskScr = leskScr + count**2
+     #           flag = 0
+                count = 0
+                incmt2 = 0
+            else:
+                incmt2 = incmt2 + 1
+    if count>0:
+        leskScr = leskScr + count**2
+
+    return leskScr
+if __name__ == "__main__":
+  #  s1 = input("enter the first sentence")
+   # s2 = input("enter the sencond sentence")
+    print (lesk("paper  that is specially prepared for use in drafting","the art of transferring designs specially prepared paper to wood or glass or metal surface"))
+    
